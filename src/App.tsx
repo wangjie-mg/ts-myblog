b8f3883345d31d8d51d23cac2ede1793a7e093a6
wangjie <1477497597@qq.com>
2020-12-17 13:03:16 +0800

    增加antdesign的样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router,Switch} from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
-
+import { RouteWithSubRoutes,routes } from './Router';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {
+          /* eslint-disable array-callback-return */
+          routes.map((item,i) =>{
+            return <RouteWithSubRoutes key={i} {...item}/>
+          },{})
+        }                 
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
